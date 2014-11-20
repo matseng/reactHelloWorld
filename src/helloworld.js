caf62c53fd7913915936d72ffd3fe007a4f3051c
Michael Tseng <matseng@gmail.com>
2014-11-19 23:11:35 -0800

    created basic n nav panel that saves state in parent element

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,35 +1,54 @@
 //props, state, setState, render
 
-// var NavPanel = React.createClass({
-//   render: function() {
-//     return
-//   }
-// });
-
 var NavButton = React.createClass({
-  getInitialState: function() {
-    this.setDefaultProps();
-    return null;
-  },
 
-  setDefaultProps: function() {
-    this.props.name = this.props.name || "My Button";
-    this.props.backgroundColor = this.props.backgroundColor || 'lightgray';
-    console.log(this.props);
+  setStyle: function() {
+    this.style = {
+      backgroundColor: this.props.backgroundColor || 'lightgray',
+      padding: '10px', 
+    };
   },
 
   onClick: function() {
-    console.log(this.props.name);
+    if (this.props.onClick) this.props.onClick();
   },
 
   render: function() {
     var self = this;
-    var style = {backgroundColor: self.props.backgroundColor};
-    console.log(style);
-    return <span style={style} onClick={self.onClick}>{this.props.name}</span>
+    this.setStyle();
+    this.style.color = self.props.selected ? 'blue' : 'black';
+    return <span style={self.style} onClick={self.onClick}>{this.props.name}</span>
   }
 
 });
 
-React.render(<NavButton name='Home'/>, 
+var NavPanel = React.createClass({
+  
+  getInitialState: function() {
+    return {selected: 'Home'};
+  },
+
+  onClick: function(buttonName) {
+    this.setState({selected: buttonName});
+  },
+
+  render: function() {
+    var self = this;
+    return (
+      <div>
+        {this.props.buttons.map(function(button, i) {
+          var selected = (button.name === self.state.selected) ? true : false;
+          console.log(selected);
+          return <NavButton key={i} name={button.name} selected={selected} onClick={self.onClick.bind(self, button.name)}/>;
+        })}
+      </div>
+    );
+  }
+
+});
+
+// React.render(<NavButton name='Home'/>, 
+//   document.getElementById('nav-panel'));
+
+React.render(<NavPanel buttons={ [{name: 'Home'}, {name: 'Feed'}, {name: 'New'}, {name: 'Search'}, {name: 'Recent'}] }/>,
   document.getElementById('nav-panel'));

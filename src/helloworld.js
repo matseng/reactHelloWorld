8824fb644eeeffbdb42ed5b43069d621485b1868
Michael Tseng <matseng@gmail.com>
2014-11-19 20:14:48 -0800

    working on build process

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,4 +1,22 @@
-React.render(
-  <h1>Hello, world! </h1>,
-  document.getElementById('example')
-);
+//state, props, getInitialState, setState, componentDidMount, componentWillUnmount, render
+
+function helloWorld() {
+  console.log("hello world");
+};
+
+// var greeting = React.createClass({
+  
+//   getInitialState: function() {
+//     return {name: props.name, greeting: props.greeting};
+//   },
+
+//   render: function() {
+//     console.log(greeting + ", " + name);
+//   }
+
+// });
+
+// React.renderComponent(<greeting name='mikey' greeting='hellooo'/>, 
+//   document.getElementById('example'));
+
+
